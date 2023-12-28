6fa9182334832247e79e56577edd0900741fdd0d
Arghya <arghyadutta080@gmail.com>
2023-12-29 01:47:05 +0530

    Project set up with React + Vite + TS + Tailwind + Firebase

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1 className="text-red-900">Hello</h1>
+    </>
+  );
+};
+
+export default App;
