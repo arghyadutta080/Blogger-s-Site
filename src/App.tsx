b550cefeb4a6197d65bee50f1d249a1e5433c2ce
Arghya <arghyadutta080@gmail.com>
2023-12-30 01:33:28 +0530

    isAuth functionality added and async-behaviour resolved by turning functions into Promises

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import { signIn, logOut } from "./Firebase/Auth";
+import { signIn, logOut, isAuthenticated } from "./Firebase/Auth";
+
 
 const App: React.FC = () => {
+
+  const signInUser = async () => {
+    const userInfo = await signIn()
+    console.log("Inside signInUser ", userInfo);
+  }
+
+  const checkAuth = async () => {
+    const authState: any = await isAuthenticated();
+    if (authState.authState) {
+      console.log("user is authenticated")  
+    } else {
+      console.log("inside checkAuth", authState.authState);
+    } 
+  };
+
+  const logOutUser = async () => {
+    const authState: any = await logOut();
+    console.log("inside logoutUser", authState.authState);
+  }
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
   return (
     <>
       <h1 className="text-red-900">Hello</h1>
 
-      <button onClick={() => signIn()} className="border border-black p-4">
+      <button onClick={() => signInUser()} className="border border-black p-4">
         Sign IN
       </button>
 
-      <button onClick={() => logOut()} className=" border border-red-600 p-4">
+      <button onClick={() => logOutUser()} className=" border border-red-600 p-4">
         Logout
       </button>
     </>
--- src/Firebase/Auth.ts
+++ src/Firebase/Auth.ts
@@ -9,16 +9,17 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 
-interface userInfo {
-    uid: string | null, 
-    displayName: string | null, 
-    email: string | null, 
-    photoURL: string | null
+export interface userInfo {
+    uid: string | null,
+    displayName: string | null,
+    email: string | null,
+    photoURL: string | null,
+    authState?: boolean
 }
 
 
 const addUser = async (userInfo: userInfo) => {
-    try {     
+    try {
         const userRef = collection(db, "users");
         await setDoc(doc(userRef, userInfo.email || ""), userInfo);
     } catch (error) {
@@ -30,8 +31,46 @@ const addUser = async (userInfo: userInfo) => {
 const signIn = () => {
     const provider = new GoogleAuthProvider();
 
-    signInWithPopup(auth, provider)                     // signInWithPopup pops up the google account auth window
-        .then(() => {
+    return new Promise((resolve, reject) => {
+
+        signInWithPopup(auth, provider)                     // signInWithPopup pops up the google account auth window
+            .then((result) => {
+                const user = result.user
+                if (user) {
+                    const uid = user.uid;
+                    const displayName = user.displayName;
+                    const email = user.email;
+                    const photoURL = user.photoURL;
+
+                    const userInfo = { uid, displayName, email, photoURL, authState: true };
+
+                    addUser(userInfo)                   // storing `userInfo` in form of a document in "users" collection
+
+                    console.log("Signed In user details ", userInfo);
+
+                    resolve(userInfo);
+
+                } else {
+                    resolve({ authState: false });
+                }
+            })
+            .catch((error: any) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                const email = error.customData.email;
+                const credential = GoogleAuthProvider.credentialFromError(error);
+
+                console.log(errorCode, errorMessage, email, credential);
+            });
+    })
+};
+
+
+const isAuthenticated = () => {
+
+    return new Promise((resolve, reject) => {
+
+        try {
             onAuthStateChanged(auth, (user) => {        // onAuthStateChanged function returns the user info 
                 if (user) {                             // and handle the functionality after selecting the google account from auth window
                     const uid = user.uid;
@@ -39,46 +78,38 @@ const signIn = () => {
                     const email = user.email;
                     const photoURL = user.photoURL;
 
-                    const userInfo = { uid, displayName, email, photoURL };
+                    const userInfo: userInfo = { uid, displayName, email, photoURL, authState: true };
 
-                    addUser(userInfo)                   // storing `userInfo` in form of a doc in "users" collection
-
-                    console.log(userInfo);
-
-                    // add code to change auth state to `true` and update userInfo state in React code
-
+                    resolve(userInfo);
                 } else {
-                    // add code to keep auth state `false` and userInfo state blank `{}` in React code
+                    resolve({ authState: false });
                 }
-            });
-        })
-        .catch((error: any) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            const email = error.customData.email;
-            const credential = GoogleAuthProvider.credentialFromError(error);
+            })
 
-            console.log(errorCode, errorMessage, email, credential);
-        });
-};
-
-
-const logOut = () => {
-    try {  
-        onAuthStateChanged(auth, (user)=>{
-             if (user) {
-                 signOut(auth).then(() => {
-                     console.log("user signed out")
-
-                     // add code to change auth state to `false` and update userInfo state in React code
-
-                 })
-             }
-         })
-    } catch (error) {
-        console.log(error);
-    }
+        } catch (error) {
+            resolve({ authState: false });
+        }
+    })
 }
 
 
-export { signIn, logOut };
+const logOut = () => {
+    return new Promise((resolve, reject) => {
+
+        try {
+            const user = auth.currentUser;
+            if (user !== null) {
+                signOut(auth).then(() => {
+                    console.log("user signed out")
+                    resolve({ authState: false });
+                })
+            }
+            
+        } catch (error) {
+            console.log(error);
+        }
+    })
+}
+
+
+export { signIn, logOut, isAuthenticated };
