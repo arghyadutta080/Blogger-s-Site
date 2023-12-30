36b1363f0d30d82fae0074a294540974c2769007
Arghya <arghyadutta080@gmail.com>
2023-12-31 02:56:37 +0530

    Image resizing functionality modified in TextEditor

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,45 +1,62 @@
 import React, { useState } from "react";
-import ReactQuill from "react-quill";
+import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { ImageResize } from "quill-image-resize-module-ts";
+import BlotFormatter from "quill-blot-formatter";
+
+Quill.register("modules/imageResize", ImageResize);
+Quill.register("modules/blotFormatter", BlotFormatter);
 
 const TextEditor: React.FC = () => {
-  const [value, setValue] = useState("");
+  const [htmlText, setHtmlText] = useState("");
 
   var toolbarOptions = [
     ["bold", "italic", "underline", "strike"], // toggled buttons
     ["blockquote", "code-block"],
-
     [{ header: 1 }, { header: 2 }], // custom button values
     [{ list: "ordered" }, { list: "bullet" }],
     [{ script: "sub" }, { script: "super" }], // superscript/subscript
     [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
     [{ direction: "rtl" }], // text direction
-    ["link", "image"],[
-      // [{ size: ["small", false, "large", "huge"] }], // custom dropdown
-      { header: [1, 2, 3, 4, 5, 6, false] }
-    ],
-
-    [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+    ["link", "image", "video"],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ color: [] }, { background: [] }], // dropdown with defaults from theme 
     [{ font: [] }],
     [{ align: [] }],
-
     ["clean"], // remove formatting button
   ];
 
   const modules = {
-    toolbar: toolbarOptions
-  }
+    toolbar: toolbarOptions,
+    clipboard: { matchVisual: false },      // toggle to add extra line breaks when pasting HTML:
+    imageResize: {
+      parchment: Quill.import("parchment"),
+      modules: ["Resize", "DisplaySize"],
+    },
+    blotFormatter: {},
+  };
 
-  const PrintValue = () => {
-    console.log(value);
-  }
+  const PrintHtmlText = () => {
+    console.log(htmlText);
+  };
 
   return (
     <div>
-      <ReactQuill theme="snow" modules={modules} value={value} onChange={setValue} className=" h-52 w-2/3 border-2 border-red-700 m-10 flex flex-col"/>;
-      <button onClick={() => PrintValue()} >Print</button>
+      <ReactQuill
+        theme="snow"
+        modules={modules}
+        value={htmlText}
+        onChange={setHtmlText}
+        className=" h-auto w-3/4 border-2 border-red-700 m-10 flex flex-col"
+      />
+      <button
+        onClick={() => PrintHtmlText()}
+        className=" p-3 rounded-md border-2 border-blue-600"
+      >
+        Print
+      </button>
     </div>
-  ); 
+  );
 };
 
 export default TextEditor;
